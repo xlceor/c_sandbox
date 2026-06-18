7b94b3c52a53b1832e07fe12f9fbff5d7707d77c
Carlos Osorio <gimcode6@gmail.com>
2026-06-18 13:56:44 -0600

    Add -a flag and refactor listing logic
    Introduce support for -a to show hidden files, add global flags (onlydir, onlyfiles, showhide), and validate that -d and -f are not used together. Refactor printing into preprocess() and printitems(), move opendir() until after argument parsing, and improve error handling. Updated the myls binary and source to reflect these behavioral and structural changes.

--- ls/myls.c
+++ ls/myls.c
@@ -3,8 +3,11 @@
 #include <string.h>
 #include <dirent.h>
 
+bool onlydir = false;
+bool onlyfiles = false;
+bool showhide = false;
 
-void printdir(DIR *dir, bool onlydir, bool onlyfiles);
+void preprocess(DIR *dir);
 
 
 int main(int argc, char *argv[])
@@ -12,21 +15,16 @@ int main(int argc, char *argv[])
     DIR *dir;
 
     char* dirpath = ".";
-    dir = opendir(dirpath);
-    if (dir == NULL)
-    {
-        perror("No se pudo acceder a la carpeta");
-        return 1;
-    }
 //    while((entrada = readdir(dir)) != NULL)
 //    {
 //        printf("%s\n", entrada->d_name);
 //    }
-    bool onlydir = false;
-    bool onlyfiles = false;
 
     for (int i = 1; i < argc; i++)
     {
+        if (strcmp(argv[i], "-a") == 0){
+            showhide = true;
+        }
         if (strcmp(argv[i], "-d") == 0)
         {
             onlydir = true;
@@ -35,34 +33,55 @@ int main(int argc, char *argv[])
         {
             onlyfiles = true;
         }
+        if (onlyfiles && onlydir){
+            perror("No se pueden filtrar por archivos y directorios al mismo tiempo\n");
+            return 1;
+        } else {
+            dirpath = argv[i];
+        }
 
     }
 
-    printdir(dir, onlydir, onlyfiles);
-
-
+    dir = opendir(dirpath);
+    if (dir == NULL)
+    {
+        perror("No se pudo acceder a la carpeta");
+        return 1;
+    }
+    preprocess(dir);
     closedir(dir);
     return 0;
 }
 
-void printdir(DIR *dir, bool onlydir, bool onlyfiles)
+void printitems(char *entrada)
+{
+    if(!showhide){
+        if (entrada[0] != '.'){
+            printf("%s\n", entrada);
+        }
+    } else {
+        printf("%s\n", entrada);
+    }
+}
+
+void preprocess(DIR *dir)
 {
     struct dirent *entrada;
     while ((entrada = readdir(dir)) != NULL){
         if (onlydir){
             if (entrada->d_type == DT_DIR)
             {
-                printf("%s\n", entrada->d_name);
+                printitems(entrada->d_name);
             }
         } else if (onlyfiles){
             if (entrada->d_type == DT_REG)
             {
-                printf("%s\n", entrada->d_name);
+                printitems(entrada->d_name);
             }
         }
         else
         {
-            printf("%s\n", entrada->d_name);
+            printitems(entrada->d_name);
         }
     }
 }
